e5ebe5de123ea6ff948c3e0c4ad6fe45bc889081
LeoKHoward <leo-howard@hotmail.co.uk>
2020-10-26 16:21:05 +0000

    Refactored code, more tests

--- src/main/java/com/example/changecalculator/ChangeCalculatorApplication.java
+++ src/main/java/com/example/changecalculator/ChangeCalculatorApplication.java
@@ -5,29 +5,35 @@ import java.util.Scanner;
 public class ChangeCalculatorApplication {
 
     public static void main(String[] args) throws Exception {
-        String currency;
 
-        Scanner inputScanner = new Scanner(System.in);
+        new Scanner(System.in);
 
-        // Choose currency
-        currency = getCurrencyType(inputScanner);
+        String currency = getCurrencyType();
 
-        purchaseQuestions(currency, inputScanner);
+        purchaseQuestions(currency);
 
     }
 
-    private static String getCurrencyType(Scanner inputScanner) {
-        String currency;
+    public static String getCurrencyType() throws Exception {
+
         System.out.println("\nWill you be purchasing something with £ or $: ");
 
-        currency = inputScanner.next();
+        Scanner inputScanner = new Scanner(System.in);
+
+        String currency = inputScanner.next();
+
+        if ((!currency.equals("£")) && (!currency.equals("$"))) {
+            throw new Exception("Invalid currency type entered!");
+        }
 
         return currency;
     }
 
-    private static void purchaseQuestions(String currency, Scanner inputScanner) throws Exception {
+    public static void purchaseQuestions(String currency) {
         double totalCost;
         double moneyPaid;
+
+        Scanner inputScanner = new Scanner(System.in);
 
         System.out.println("\nHow much does the purchase cost: ");
         totalCost = inputScanner.nextDouble() * 100;
@@ -40,16 +46,12 @@ public class ChangeCalculatorApplication {
 
         } else if (currency.equals("$")) {
             workoutChangeInUsd(totalCost, moneyPaid);
-
-        } else {
-            throw new Exception("Invalid currency type entered!");
         }
     }
 
     public static class BankNoteOrCoin {
         public final double value;
         public final String description;
-
 
         public BankNoteOrCoin(double value, String description) {
             this.value = value;
@@ -79,7 +81,6 @@ public class ChangeCalculatorApplication {
                     new BankNoteOrCoin(0.05, " 5p coin(s)"),
                     new BankNoteOrCoin(0.02, " 2p coin(s)"),
                     new BankNoteOrCoin(0.01, " 1p coin(s)")
-
             };
 
             bankNoteOrCoin(changeGbp, gbpBankNotesOrCoins);
@@ -87,7 +88,6 @@ public class ChangeCalculatorApplication {
         } else {
             System.out.println("\nNot enough money you cheap bastard!");
         }
-
     }
 
     public static void workoutChangeInUsd(double totalCostUsd, double moneyPaidUsd) {
@@ -110,7 +110,6 @@ public class ChangeCalculatorApplication {
                     new BankNoteOrCoin(0.1, " dime coin(s)"),
                     new BankNoteOrCoin(0.05, " nickel coin(s)"),
                     new BankNoteOrCoin(0.01, " one cent coin(s)")
-
             };
 
             bankNoteOrCoin(changeUsd, usdBankNotesOrCoins);
@@ -118,16 +117,15 @@ public class ChangeCalculatorApplication {
         } else {
             System.out.println("\nNot enough money you cheap bastard!");
         }
-
     }
 
     private static void bankNoteOrCoin(double changeUsd, BankNoteOrCoin[] usdBankNotesOrCoins) {
 
         for (BankNoteOrCoin bankNoteOrCoin : usdBankNotesOrCoins) {
-            int noteTotal = (int) ((int) changeUsd / bankNoteOrCoin.value);
-            if (noteTotal > 0) {
+            int noteOrCoinTotal = (int) ((int) changeUsd / bankNoteOrCoin.value);
+            if (noteOrCoinTotal > 0) {
                 changeUsd = changeUsd % (int) (bankNoteOrCoin.value * 100);
-                System.out.println(noteTotal / 100 + bankNoteOrCoin.description);
+                System.out.println(noteOrCoinTotal / 100 + bankNoteOrCoin.description);
             }
         }
     }
